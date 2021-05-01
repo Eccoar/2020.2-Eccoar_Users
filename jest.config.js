5db2e6e27d29fbda4e63d63c209ba8573ae63e12
Guilherme <marquesguilherme3@gmail.com>
2021-05-01 06:29:19 -0300

    Add helm chart and gitlabci

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	testPathIgnorePatterns: ['<rootDir>/build'],
 	collectCoverage: true,
-	testPathIgnorePatterns: ['build'],
+	testResultsProcessor: 'jest-sonar-reporter',
+	coveragePathIgnorePatterns: ['/node_modules/', '/test/', '/db/'],
 	moduleNameMapper: {
 		'@controllers/(.*)': '<rootDir>/src/controllers/$1',
+		'@entity/(.*)': '<rootDir>/src/entity/$1',
+		'@repositories/(.*)': '<rootDir>/src/repositories/$1',
 		'@services/(.*)': '<rootDir>/src/services/$1',
 		'@utils/(.*)': '<rootDir>/src/utils/$1',
 	},
