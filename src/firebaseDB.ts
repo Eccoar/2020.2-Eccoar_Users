47a42b3a8c50888492b6d28d77b4104ad89e4d16
Guilherme <marquesguilherme3@gmail.com>
2021-05-01 19:30:20 -0300

    Remove comments

--- src/firebaseDB.ts
+++ src/firebaseDB.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
 
-// @ts-expect-error file must not be commited, so commits should not be blocked by this line
 import serviceAccount from '../service-account.json';
 
 const firebaseCred = serviceAccount as ServiceAccount;
