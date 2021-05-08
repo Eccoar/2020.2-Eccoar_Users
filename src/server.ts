23f72c641e09f9a39494be64f6d795af8c0439e8
Arthur-Matos <git.arthurmatos@gmail.com>
2021-05-08 11:48:55 -0300

    Configure morgan

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import routers from './routes';
 import handleErrors from '@utils/ErrorHandler';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
 app.use(express.json());
+app.use(morgan('combined'));
 app.use(cors());
 app.use(routers);
 app.use(handleErrors);
